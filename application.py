a90c1f4262bf774f263cb99c5f4b66a69dbf19da
Gokul PM <contact@gokulpm.com>
2021-10-09 19:28:21 +0530

    updated menu with while loop

--- application.py
+++ application.py
@@ -7,12 +7,12 @@ def login():
     key = get_key(r'application_key.json')
     choice = login_menu()
     if choice == 'l':
-        input_username = input('Username: ')
+        username = input('Username: ')
         database = read(r'database.json')
         flag = False
-        for key_record in database_dictionary:
+        for key_record in database:
             decrypted_key_record == (key.decrypt(key_record.encode())).decode()
-            if input_username == decrypted_key_record:
+            if username == decrypted_key_record:
                 flag = True
                 break
         if flag:
@@ -23,12 +23,19 @@ def login():
                 user_key = b64encode(os.urandom(16))
                 print('Setup the following key in the Google Authenticator app')
                 print('key: {0}'.format(user_key.decode()))
+                choice = navigation()
+                if choice()
                 vault_key = b64encode(os.urandom(16))
                 vault_name = r'{0}.json'.format(vault_key.decode())
-                else:
-                    sys.exit()
+                username = lock(username, key)
+                user_key = lock(user_key, key)
+                vault_name = lock(vault_name, key)
+                database.update({username:[user_key, vault_name]})
+                write(r'database.json', database)
             if choice == 'n':
-                sys.exit()                      
+                break
+            if choice == 'e':
+                sys.exit()
     if choice == 'e':
         sys.exit()
         
--- menu.py
+++ menu.py
@@ -1,11 +1,23 @@
 def login_menu() -> str:
-    choice = (input('l -> login, e -> exit\n')).lower()
+    choice = None
+    while choice not in ['l', 'e']:
+        choice = (input('l -> login, e -> exit\n')).lower()
     return choice
 
 def main_menu() -> str:
-    choice = (input('s -> store, u -> update, r -> retrieve, l -> logout, e -> exit\n')).lower()
+    choice = None
+    while choice not in ['s', 'u', 'r', 'l', 'e']:
+        choice = (input('s -> store, u -> update, r -> retrieve, l -> logout, e -> exit\n')).lower()
     return choice
 
 def new_user() -> str:
-    choice = (input('Create new user? (y/n)\n')).lower()
+    choice = None
+    while choice not in ['y', 'n', 'e']:
+        choice = (input('Create new user?\ny -> yes, n -> no, e -> exit\n')).lower()
+    return choice
+
+def navigation() -> str:
+    choice = None
+    while choice not in ['c', 'a', 'e']:
+        choice = (input('c -> continue, a -> abort, e -> exit')).lower()
     return choice
