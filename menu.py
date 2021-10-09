00f1d801e9cf1b22bfe6ecb32dd3f610570afd1c
Gokul PM <contact@gokulpm.com>
2021-10-09 17:48:32 +0530

    menu 0.1

---
+++ menu.py
@@ -0,0 +1,5 @@
+def login_menu():
+    choice = (input('l -> login, e -> exit\n\n')).lower()
+
+def main_menu():
+    choice = (input('s -> store, u -> update, r -> retrieve, l -> logout, e -> exit\n\n')).lower()
